b93d745a44a3ec2e590a72b320429527972cb4dc
Efrem Yohanis <efremyohanis116@gmial.com>
2022-11-25 10:21:34 +0300

    user profile updated

--- Store_manager/urls.py
+++ Store_manager/urls.py
@@ -5,4 +5,12 @@ urlpatterns = [
    path('manage_catagory',views.manage_catagory,name="manage-catagory"),
    path('add_to_store',views.add_to_store,name="add-to-store"),
    path('cheek_request',views.cheeck_request,name="cheeck-request"),
+   # -------- profile --------- #
+   path('user_Profile',views.user_Profile,name="user-Profile"),
+   path('edit_Profile',views.edit_Profile,name="edit-Profile"),
+   path('chage_password',views.chage_password,name="chage_password"),
+   path('chage_profile_pic',views.chage_profile_pic,name="chage-profile-pic"),
+   # -------- End profile --------- #
+
+
 ]
--- Store_manager/views.py
+++ Store_manager/views.py
@@ -1,14 +1,41 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.shortcuts import render, redirect, reverse
+from .form import *
+from .models import *
+from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
 
 def store_dashboard(request):
     return render(request,'Store_manager/dashboard.html')
-
 def manage_catagory(request):
     return render(request,'Store_manager/Catagory/manage_catagory.html')
 def add_to_store(request):
     return render(request,"Store_manager/Add_to_Store/add_to_store.html")
-
 def cheeck_request(request):
     return render(request,"Store_manager/cheeck_Request/cheeck_request.html")
+def user_Profile(request):
+    return render(request,'Store_manager/Profile/show_profile.html')
+def edit_Profile(request):
+    return render(request,'Store_manager/Profile/edit_profile.html',)
+def chage_password(request):
+    if request.method == 'POST':
+        form = passwordform(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(
+                request, 'Your password was successfully updated!')
+            return redirect('show_profile_agent')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = passwordform(request.user)
+        context = {
+            'form': form
+            }
+    return render(request,'Store_manager/Profile/chage_pass.html')
+
+def chage_profile_pic(request):
+    return render(request,'Store_manager/Profile/change_profile_pic.html')
