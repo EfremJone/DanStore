6f295c1c44821ff744eefad38d30afce4a50ac28
Efrem Yohanis <efremyohanis116@gmial.com>
2022-12-06 11:41:38 +0300

    chage made in chatbot

--- Store_manager/models.py
+++ Store_manager/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Catagory(models.Model):
@@ -74,4 +74,12 @@ class form2permanent(models.Model):
 class student(models.Model):
     name=models.CharField(max_length=20,null=True,blank=True)
 
-    
+
+class chatbot(models.Model):
+    me_with=models.ForeignKey(User,null=True,blank=True, on_delete=models.CASCADE)
+    me=models.CharField(max_length=200,null=True,blank=True)
+    message=models.TextField(max_length=1000,null=True,blank=True)
+    date=models.DateTimeField(auto_now_add=True)
+    checked=models.BooleanField(default=False)
+    def __str__(self):
+        return str(self.date)
