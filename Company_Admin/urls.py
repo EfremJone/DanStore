96c95535a66e6275481d9634d04bf9fa6bbfe44b
Milli-saved <million12.tenkir@gmail.com>
2022-12-08 15:18:20 +0300

    store fetch is done.

--- Company_Admin/urls.py
+++ Company_Admin/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('store', views.store, name='store'),
     path('store-details', views.store_details, name='store-details'),
     path('add-new-store',views.add_new_store,name='add-new-store'),
-    path('cat-item-detail',views.cat_item_detail,name='cat-item-detail'),
+    path('cat-item-detail/<int:id>',views.cat_item_detail,name='cat-item-detail'),
 
     # 
     path('reports', views.reports, name='reports'),
