caac0afc62b6a665a806b29c7894c8839c6d59fc
Milli-saved <million12.tenkir@gmail.com>
2022-12-19 08:53:12 +0300

    structural correction.

--- Department_Head/views.py
+++ Department_Head/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.contrib import messages
+
 def dept_head(request):
     
-    return render(request,'Department_head/index.html',)
+    return render(request,'Department_head/Dashboard/index.html',)
 
 def add_employe(request):
-    return render(request,'Department_head/add_employe.html',)
+    return render(request,'Department_head/AddNewEmployee/index.html',)
 
 def approve_request(request):
-    return render(request,'Department_head/check_request.html',)
+    return render(request,'Department_head/CheckRequest/index.html',)
 
 def make_request(request):
-    return render(request,'Department_head/make_request.html',)
+    return render(request,'Department_head/MakeRequest/index.html',)
